c0db49f8b1d3e79029950c13b7f6c1baf6127cb0
J-Yoshigai <dpsknu25@gmail.com>
2020-10-11 07:26:56 +0900

    add --all

---
+++ resources/views/contactUs.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>お問い合わせ</title>
+</head>
+<body>
+    <form>
+        <label name="name">お名前</label>
+        <input name="name">
+
+    </form>
+</body>
+</html>
